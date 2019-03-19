0e1bc958bba57a4924055051438039dc90f55729
dbarthe <barthelemy.delemotte@gmail.com>
2019-03-19 23:55:57 +0100

    try to make github a helm repo

--- update-helm.sh
+++ update-helm.sh
@@ -3,4 +3,4 @@
 
 (cd helm ; helm package azure-snapshoter)
 
-helm repo index helm --url https://github.com/DBarthe/azure-snapshoter.sh/tree/master/helm
+helm repo index helm --url https://dbarthe.github.io/azure-snapshoter.sh/helm
